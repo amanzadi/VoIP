8516904718cd16cb04e86d2cb15a449083562a85
Amir H. Amanzadi <47634201+amanzadi@users.noreply.github.com>
2023-04-01 23:41:55 +0330

    Update install-asterisk.sh

--- install-asterisk.sh
+++ install-asterisk.sh
@@ -1,14 +1,14 @@
 sudo apt update && sudo apt upgrade
 sudo apt install wget build-essential subversion
 cd /usr/src/
-sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-18-current.tar.gz
+sudo wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-20-current.tar.gz
 sudo tar zxf asterisk-18-current.tar.gz
-cd asterisk-18.*/
+cd asterisk-20.*/
 sudo contrib/scripts/get_mp3_source.sh
 sudo contrib/scripts/install_prereq install
 sudo ./configure
 
-
+# select the mp3 menu then save and exit
 sudo make menuselect
 sudo make -j2
 sudo make install
@@ -19,4 +19,5 @@ sudo ldconfig
 sudo systemctl start asterisk
 sudo asterisk -vvvr
 
+sudo ufw enable
 sudo ufw allow 5060/udp
